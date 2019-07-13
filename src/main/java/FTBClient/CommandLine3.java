5d7df306786bbc0128aa9a976e2fd11d334a869d
charboltron <boltch@pdx.edu>
2019-07-12 17:59:15 -0700

    Oops. Changed the name of the file to CommandLine3.java

--- src/main/java/FTBClient/CommandLine3.java
+++ src/main/java/FTBClient/CommandLine3.java
@@ -7,7 +7,7 @@ import java.io.*;
 //import java.lang.invoke.DirectMethodHandle$Holder;
 import java.util.*;
 
-public class CommandLine2 {
+public class CommandLine3 {
 
     public static void main(String[] args){
 
