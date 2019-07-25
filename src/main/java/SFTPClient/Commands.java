37a2a32964ea2361489708602847b42a7cf886b3
Tom <Tom@charless-MacBook-Pro.local>
2019-07-24 17:03:21 -0700

    INCOMPLETE - copy directory function

--- src/main/java/SFTPClient/Commands.java
+++ src/main/java/SFTPClient/Commands.java
@@ -178,5 +178,72 @@ public class Commands {
             return;
         }
     }
+    public void copyRemoteDirectory(ChannelSftp sftpChannel) throws SftpException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the source directory to copy: ");
+        String src = scanner.nextLine().trim();
+        File srcFolder = new File(src);
+        System.out.println("Enter the destination directory: ");
+        String dest = scanner.nextLine().trim();
+        File destFolder = new File(dest);
+
+        if(!srcFolder.exists()) {
+            System.out.println("Directory does not exist.");
+        }
+        try{
+            if(srcFolder.isDirectory()){
+
+                //if directory not exists, create it
+                if(!destFolder.exists()){
+                    dest.mkdir();
+                    System.out.println("Directory copied from "
+                            + src + "  to " + dest);
+                }
+
+                //list all the directory contents
+                String files[] = src.list();
+
+                for (String file : files) {
+                    //construct the src and dest file structure
+                    File srcFile = new File(src, file);
+                    File destFile = new File(dest, file);
+                    //recursive copy
+                    copyFolder(srcFile,destFile);
+                }
+
+            }else{
+                //if file, then copy it
+                //Use bytes stream to support all file types
+                InputStream in = new FileInputStream(src);
+                OutputStream out = new FileOutputStream(dest);
+
+                byte[] buffer = new byte[1024];
+
+                int length;
+                //copy the file content in bytes
+                while ((length = in.read(buffer)) > 0){
+                    out.write(buffer, 0, length);
+                }
+
+                in.close();
+                out.close();
+                System.out.println("File copied from " + src + " to " + dest);
+            }
+        }
+        }catch(IOException e){
+            e.printStackTrace();
+            //error, just exit
+            System.exit(0);
+        }
+        String workingDir = sftpChannel.pwd();
+        Vector fileList = sftpChannel.ls(workingDir);
+        if (fileList.contains(afterFile)){
+            System.out.println("Error. There is already a file with that name!");
+            return;
+        }else{sftpChannel.rename(beforeFile, afterFile);
+            System.out.println("Rename was successful!");
+            return;
+        }
+
 }
 
